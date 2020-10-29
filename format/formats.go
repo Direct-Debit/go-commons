c2fb1892cf7548c94865cfce5cc3dd00e19e6278
Arie De Bok <arie@directdebit.co.za>
2020-10-29 15:42:48 +0200

    Add another date format

--- format/formats.go
+++ format/formats.go
@@ -5,6 +5,7 @@ import "fmt"
 const (
 	DateShort6          = "060102"
 	DateShort6Slashes   = "06/01/02"
+	DateShort8          = "20060102"
 	DateShortSlashes    = "2006/01/02"
 	DateShortDashes     = "2006-01-02"
 	DateTimeShort       = "02/01/2006 15:04"
