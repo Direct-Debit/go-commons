1048c2e1228f7b37cb6b28448734469cb480df8e
Arie De Bok <arie@directdebit.co.za>
2020-11-02 16:12:20 +0200

    Add performance logging middleware

--- webutil/web.go
+++ webutil/web.go
@@ -2,8 +2,11 @@ package webutil
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/Direct-Debit/go-commons/errlib"
+	log "github.com/sirupsen/logrus"
 	"net/http"
+	"time"
 )
 
 func ErrorResponder(msg string, code int) http.HandlerFunc {
@@ -15,4 +18,14 @@ func ErrorResponder(msg string, code int) http.HandlerFunc {
 			Error string `json:"error"`
 		}{Error: msg}), "Couldn't encode JSON error")
 	}
+}
+
+func PerformanceLogMiddleWare(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Info(r.URL)
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		duration := time.Since(start)
+		log.Info(fmt.Sprintf("Handled in %v seconds", duration.Seconds()))
+	})
 }
