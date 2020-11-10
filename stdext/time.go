fe141ef93519023b6d830ce0a3f5c0db63242b69
Arie De Bok <arie@directdebit.co.za>
2020-11-10 15:39:55 +0200

    Force dates to be in UTC

--- stdext/time.go
+++ stdext/time.go
@@ -2,7 +2,7 @@ package stdext
 
 import "time"
 
-// return a copy of the given time, but with hours, minutes, seconds and nanoseconds set to 0
+// return a copy of the given time, but with hours, minutes, seconds and nanoseconds set to 0. Timezone is set to UTC
 func Date(in time.Time) time.Time {
-	return time.Date(in.Year(), in.Month(), in.Day(), 0, 0, 0, 0, in.Location())
+	return time.Date(in.Year(), in.Month(), in.Day(), 0, 0, 0, 0, time.UTC)
 }
