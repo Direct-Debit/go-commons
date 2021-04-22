4165012e65c846e6b99d652099365b0740163371
Arie De Bok <arie@directdebit.co.za>
2021-04-22 16:31:58 +0200

    Add cooler print interface

--- errlib/errors.go
+++ errlib/errors.go
@@ -2,12 +2,11 @@ package errlib
 
 import (
 	"fmt"
-	log "github.com/sirupsen/logrus"
 )
 
-func Error(printer func(...interface{}), err error, format string, a ...interface{}) bool {
+func Error(printer func(string), err error, format string, a ...interface{}) bool {
 	if err != nil {
-		msg := fmt.Sprintf(format, a)
+		msg := fmt.Sprintf(format, a...)
 		printer(fmt.Sprintf("%v: %v", msg, err))
 		return true
 	}
@@ -15,26 +14,30 @@ func Error(printer func(...interface{}), err error, format string, a ...interfac
 }
 
 func FatalError(err error, format string, a ...interface{}) {
-	Error(log.Fatal, err, format, a)
+	Error(fatalFunc, err, format, a)
 }
 
 func PanicError(err error, format string, a ...interface{}) {
-	Error(log.Panic, err, format, a)
+	Error(panicFunc, err, format, a)
 }
 
 // ErrorError checks if err != nil, in which case it logs the error with fmt.Sprintf(format, a) prepended
 func ErrorError(err error, format string, a ...interface{}) bool {
-	return Error(log.Error, err, format, a)
+	return Error(errorFunc, err, format, a)
 }
 
 func WarnError(err error, format string, a ...interface{}) bool {
-	return Error(log.Warn, err, format, a)
+	return Error(warnFunc, err, format, a)
 }
 
 func InfoError(err error, format string, a ...interface{}) bool {
-	return Error(log.Info, err, format, a)
+	return Error(infoFunc, err, format, a)
 }
 
 func DebugError(err error, format string, a ...interface{}) bool {
-	return Error(log.Debug, err, format, a)
+	return Error(debugFunc, err, format, a)
+}
+
+func TraceError(err error, format string, a ...interface{}) bool {
+	return Error(traceFunc, err, format, a)
 }
---
+++ errlib/printer.go
@@ -0,0 +1,68 @@
+package errlib
+
+import log "github.com/sirupsen/logrus"
+
+type Logger interface {
+	Fatal(string)
+	Panic(string)
+	Error(string)
+	Warn(string)
+	Info(string)
+	Debug(string)
+	Trace(string)
+}
+
+var loggers []Logger
+
+func AddLogger(l Logger) {
+	loggers = append(loggers, l)
+}
+
+func fatalFunc(msg string) {
+	for _, l := range loggers {
+		l.Fatal(msg)
+	}
+	log.StandardLogger().Fatal(msg)
+}
+
+func panicFunc(msg string) {
+	for _, l := range loggers {
+		l.Panic(msg)
+	}
+	log.StandardLogger().Panic(msg)
+}
+
+func errorFunc(msg string) {
+	for _, l := range loggers {
+		l.Error(msg)
+	}
+	log.StandardLogger().Error(msg)
+}
+
+func warnFunc(msg string) {
+	for _, l := range loggers {
+		l.Warn(msg)
+	}
+	log.StandardLogger().Warn(msg)
+}
+
+func infoFunc(msg string) {
+	for _, l := range loggers {
+		l.Info(msg)
+	}
+	log.StandardLogger().Info(msg)
+}
+
+func debugFunc(msg string) {
+	for _, l := range loggers {
+		l.Debug(msg)
+	}
+	log.StandardLogger().Debug(msg)
+}
+
+func traceFunc(msg string) {
+	for _, l := range loggers {
+		l.Trace(msg)
+	}
+	log.StandardLogger().Trace(msg)
+}
