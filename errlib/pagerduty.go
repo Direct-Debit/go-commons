b48682ebe598ff87f6ee29771f181cd1fb3348e6
Jonathan <jonathan@directdebit.co.za>
2021-04-30 12:33:41 +0200

    Update logger interface for PagerDuty

--- errlib/pagerduty.go
+++ errlib/pagerduty.go
@@ -6,10 +6,11 @@ import (
 	"github.com/PagerDuty/go-pagerduty"
 )
 
-const PAGERDUTY_ROUTING_KEY = "d7827f7d66574b06d0696b696e865442"
-
 const (
 	sevFatal = "fatal"
+	sevError = "error"
+	sevWarn  = "warning"
+	sevInfo  = "info"
 )
 
 type event_traceback struct {
@@ -20,6 +21,7 @@ type PagerDuty struct {
 	Product     string
 	Component   string
 	Environment string
+	RoutingKey  string
 }
 
 func (p PagerDuty) Fatal(s string) {
@@ -27,43 +29,35 @@ func (p PagerDuty) Fatal(s string) {
 }
 
 func (p PagerDuty) Panic(s string) {
-	panic("implement me")
+	p.createPagerdutyAlert(s, sevFatal)
 }
 
 func (p PagerDuty) Error(s string) {
-	panic("implement me")
+	p.createPagerdutyAlert(s, sevError)
 }
 
 func (p PagerDuty) Warn(s string) {
-	panic("implement me")
+	p.createPagerdutyAlert(s, sevWarn)
 }
 
 func (p PagerDuty) Info(s string) {
-	panic("implement me")
-}
-
-func (p PagerDuty) Debug(s string) {
-	panic("implement me")
-}
-
-func (p PagerDuty) Trace(s string) {
-	panic("implement me")
+	p.createPagerdutyAlert(s, sevInfo)
 }
 
 func (p PagerDuty) createPagerdutyAlert(msg string, severity string) {
 	details := event_traceback{msg}
 	event := pagerduty.V2Event{
-		RoutingKey: PAGERDUTY_ROUTING_KEY,
+		RoutingKey: p.RoutingKey,
 		Action:     "trigger",
 		Payload: &pagerduty.V2Payload{
-			Summary:  "[dps/error/DPSM]",
+			Summary:  "[dps/error/DPSM]", //Add product/component/env?
 			Source:   "DPS Monitor",
 			Severity: severity,
 			Details:  details,
 		},
 	}
-	message, res := pagerduty.ManageEvent(event)
-	if res != nil {
-		warnFunc(fmt.Sprintf("Unable to create pagerduty event! %v\n%v", res, message))
+	resp, err := pagerduty.ManageEvent(event)
+	if err != nil {
+		warnFunc(fmt.Sprintf("Unable to create pagerduty event! %v\n%v", resp, err))
 	}
 }
