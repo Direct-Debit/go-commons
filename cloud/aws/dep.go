4d3ec2526e07326f10806f4395b68763003dd44b
Arie De Bok <arie@directdebit.co.za>
2021-05-11 11:33:45 +0200

    Add more deprecated funcs for backwards compatibilty

--- cloud/aws/dep.go
+++ cloud/aws/dep.go
@@ -4,8 +4,18 @@ import (
 	"github.com/Direct-Debit/go-commons/cloud/aws/lambda"
 )
 
+// Deprecated: rather use the Client in cloud/aws/lambda
+type LambdaClient struct {
+	Client lambda.Client
+}
+
 // Deprecated: will be removed in v1 of this API
 //goland:noinspection GoUnusedExportedFunction
-func NewLambdaClient() lambda.Client {
-	return lambda.NewClient()
+func NewLambdaClient() LambdaClient {
+	return LambdaClient{lambda.NewClient()}
+}
+
+// Deprecated: rather use the Client in cloud/aws/lambda
+func (l LambdaClient) Validate(number string, branch string, accountType string) (map[string]string, error) {
+	return l.Client.Validate(number, branch, accountType)
 }
