b042b496e9abe4ea5fd46ac285ca3ba25e74a9af
Arie De Bok <arie@directdebit.co.za>
2021-05-12 14:16:05 +0200

    Add test case

--- cloud/aws/sns/sns_test.go
+++ cloud/aws/sns/sns_test.go
@@ -9,4 +9,7 @@ func TestClient_Publish(t *testing.T) {
 	cl := NewClient("arn:aws:sns:af-south-1:733171151776:dps-reports", "test")
 	err := cl.Publish("test-message", "This is a test message from Go")
 	assert.Nil(t, err)
+
+	err = cl.Publish("test-message (with parenthesis)", "This is a test message from Go. It has parenthesis in the subject line.")
+	assert.Nil(t, err)
 }
