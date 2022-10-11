0fd576b840a6b2dcb7da3aa98c89266c1076ef4c
Arie De Bok <arie@directdebit.co.za>
2022-10-11 14:10:41 +0200

    Add generic set

--- stdext/set.go
+++ stdext/set.go
@@ -26,3 +26,48 @@ func (ss StrSet) ToSlice() []string {
 	}
 	return s
 }
+
+type Set[T comparable] struct {
+	values map[T]struct{}
+}
+
+func NewSet[T comparable]() *Set[T] {
+	return &Set[T]{
+		values: make(map[T]struct{}),
+	}
+}
+
+func SetFromSlice[T comparable](s []T) *Set[T] {
+	set := &Set[T]{
+		values: make(map[T]struct{}),
+	}
+	for _, v := range s {
+		set.values[v] = struct{}{}
+	}
+	return set
+}
+
+func (s *Set[T]) Add(v T) {
+	s.values[v] = struct{}{}
+}
+
+func (s *Set[T]) Has(v T) bool {
+	_, ok := s.values[v]
+	return ok
+}
+
+func (s *Set[T]) Remove(v T) {
+	if _, ok := s.values[v]; ok {
+		delete(s.values, v)
+	}
+}
+
+func (s *Set[T]) ToSlice() []T {
+	slice := make([]T, len(s.values))
+	idx := 0
+	for v := range s.values {
+		slice[idx] = v
+		idx++
+	}
+	return slice
+}
