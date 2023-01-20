7b4b16a3637f64a23aa0e2162c75eec79e0c9666
Arie De Bok <arie@directdebit.co.za>
2023-01-20 12:53:21 +0200

    Add a generic way to cache function calls

--- stdext/maps.go
+++ stdext/maps.go
@@ -20,3 +20,23 @@ func JoinMaps(target map[string]interface{}, source map[string]interface{}) {
 		target[k] = v
 	}
 }
+
+// CachedCall tries to get a cached value from the given caching map before calling f and caching the result.
+// If f returns an error, CachedCall will return f's output without caching it.
+// CachedCall does not generate any errors of its own.
+func CachedCall[K comparable, V any](f func(K) (V, error), cache map[K]V, key K) (V, error) {
+	// Check cache and return if found
+	if v, ok := cache[key]; ok {
+		return v, nil
+	}
+
+	// call f to determine value to cache
+	v, err := f(key)
+	if err != nil {
+		return v, err
+	}
+
+	// cache value
+	cache[key] = v
+	return v, nil
+}
