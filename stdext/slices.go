c4bd685b954711d0cf2dffc856b43001fe963b33
Arie De Bok <arie@directdebit.co.za>
2023-01-24 13:59:31 +0200

    Add a array mapper that can handle errors

--- stdext/slices.go
+++ stdext/slices.go
@@ -1,5 +1,7 @@
 package stdext
 
+import "fmt"
+
 func SafeIdx[T any](idx int, arr []T) T {
 	if 0 <= idx && idx < len(arr) {
 		return arr[idx]
@@ -39,6 +41,23 @@ func Map[T any, U any](slice []T, conv func(T) U) []U {
 		result[i] = conv(t)
 	}
 	return result
+}
+
+// MapErr creates a new slice with one element for every element in the given slice.
+// The elements in the new slice will be the return values of the conversion function conv.
+// If conv returns an error, MapErr will return an error wrapping the original error
+// and a slice of the same length as the given slice containing the elements that were successfully converted before the error occurred.
+// MapErr does not generate any of its own errors.
+func MapErr[T any, U any](slice []T, conv func(T) (U, error)) ([]U, error) {
+	var err error
+	result := make([]U, len(slice))
+	for i, t := range slice {
+		result[i], err = conv(t)
+		if err != nil {
+			return nil, fmt.Errorf("failed to convert element %d: %w", i, err)
+		}
+	}
+	return result, nil
 }
 
 func SafeIdxStr(idx int, arr []string) string {
