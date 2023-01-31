1600305bba1154c3413ee54a4224e9ddeb695e4e
Arie De Bok <arie@directdebit.co.za>
2023-01-31 16:29:12 +0200

    Make stdext min and max variadic

--- stdext/numbers.go
+++ stdext/numbers.go
@@ -28,20 +28,36 @@ type Number interface {
 	Complex | RealNumber
 }
 
-// Max returns the larger of x or y.
-func Max[T RealNumber](x, y T) T {
-	if x < y {
-		return y
+// Max returns the largest number in nums, or the zero value of T if there are no nums
+func Max[T RealNumber](nums ...T) T {
+	if len(nums) == 0 {
+		var nothing T
+		return nothing
 	}
-	return x
+
+	max := nums[0]
+	for _, y := range nums {
+		if y > max {
+			max = y
+		}
+	}
+	return max
 }
 
-// Min returns the smaller of x or y.
-func Min[T RealNumber](x, y T) T {
-	if x > y {
-		return y
+// Min returns the smallest number in nums, or the zero value of T if there are no nums
+func Min[T RealNumber](nums ...T) T {
+	if len(nums) == 0 {
+		var nothing T
+		return nothing
 	}
-	return x
+
+	min := nums[0]
+	for _, y := range nums {
+		if y < min {
+			min = y
+		}
+	}
+	return min
 }
 
 func Abs[T RealNumber](x T) T {
