2f5b2052badaa856cebf97a8136049cb2b7761ac
Arie De Bok <arie@directdebit.co.za>
2023-02-15 13:15:05 +0200

    Add a rate limited worker based parallel function

--- concurrency/workerBasedParallel.go
+++ concurrency/workerBasedParallel.go
@@ -3,6 +3,7 @@ package concurrency
 import (
 	"github.com/Direct-Debit/go-commons/stdext"
 	"sync"
+	"time"
 )
 
 // Workers executes the transform function in a go routine for every input.
@@ -35,6 +36,51 @@ func Workers[I any, O any](workerCount int, input []I, transform func(I) (O, boo
 				results <- o
 			}
 		}()
+	}
+
+	wg.Wait()
+	close(results)
+
+	return stdext.ChanToSlice(results)
+}
+
+// RateLimitedWorkers will concurrently call the transform function on the given input.
+// If rate is less than or equal to 0, it will be set to one nanosecond.
+// If the input slice is of size N, RateLimitedWorkers spawns N goroutines, but no more than workerCount at a time.
+// The go routines will  also only be spawned once per tick at the given rate.
+// The bool return value for transform indicates whether the transform executed successfully.
+// Only successful transform results will be included in the result slice.
+// No ordering is guaranteed in the result slice.
+func RateLimitedWorkers[I any, O any](rate time.Duration, workerCount int, input []I, transform func(I) (O, bool)) []O {
+	if rate <= 0 {
+		rate = time.Nanosecond
+	}
+	if workerCount <= 0 {
+		workerCount = 1
+	}
+
+	results := make(chan O, len(input))
+	wg := sync.WaitGroup{}
+	workerQueue := make(chan struct{}, workerCount)
+	ticker := time.NewTicker(rate)
+
+	for _, i := range input {
+		workerQueue <- struct{}{}
+		copiedI := i
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			defer func() {
+				<-workerQueue
+			}()
+
+			o, ok := transform(copiedI)
+			if ok {
+				results <- o
+			}
+		}()
+		<-ticker.C
 	}
 
 	wg.Wait()
--- concurrency/workerBasedParallel_test.go
+++ concurrency/workerBasedParallel_test.go
@@ -44,3 +44,60 @@ func TestWorkers(t *testing.T) {
 	assert.Equal(t, 0, len(output3))
 	fmt.Println("-----------------------------------------------------")
 }
+
+func TestRateLimitedWorkers(t *testing.T) {
+	input := make([]int, 100)
+	for i := range input {
+		input[i] = i
+	}
+
+	var jobsRunning int
+	start := time.Now()
+
+	rate := time.Millisecond * 100
+	output := RateLimitedWorkers(rate, 5, input, func(i int) (int, bool) {
+		jobsRunning += 1
+		fmt.Printf("Executing job %d...\n", i)
+		fmt.Printf("Jobs running: %d\n", jobsRunning)
+		fmt.Printf("Milliseconds since start: %d\n", int(time.Since(start).Milliseconds()))
+		time.Sleep(time.Second)
+		fmt.Printf("Job %d DONE!\n", i)
+		jobsRunning -= 1
+		return 0, true
+	})
+	assert.Equal(t, 100, len(output))
+	fmt.Println("-----------------------------------------------------")
+
+	start = time.Now()
+	output2 := RateLimitedWorkers(rate, 10, input, func(i int) (any, bool) {
+		jobsRunning += 1
+		fmt.Printf("Executing job %d...\n", i)
+		fmt.Printf("Jobs running: %d\n", jobsRunning)
+		fmt.Printf("Milliseconds since start: %d\n", int(time.Since(start).Milliseconds()))
+		success := i%3 != 0
+		if success {
+			time.Sleep(time.Second)
+			fmt.Println("Success")
+		} else {
+			fmt.Println("Failure")
+		}
+		jobsRunning -= 1
+		return nil, success
+	})
+	assert.Equal(t, 66, len(output2))
+	fmt.Println("-----------------------------------------------------")
+
+	start = time.Now()
+	output3 := RateLimitedWorkers(rate, 3, input, func(i int) (struct{}, bool) {
+		jobsRunning += 1
+		fmt.Printf("Executing job %d...\n", i)
+		fmt.Printf("Jobs running: %d\n", jobsRunning)
+		fmt.Printf("Milliseconds since start: %d\n", int(time.Since(start).Milliseconds()))
+		time.Sleep(time.Millisecond * time.Duration(10*i))
+		fmt.Printf("Job %d DONE!\n", i)
+		jobsRunning -= 1
+		return struct{}{}, false
+	})
+	assert.Equal(t, 0, len(output3))
+	fmt.Println("-----------------------------------------------------")
+}
