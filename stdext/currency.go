2e12f63fd719cdc4defc37b46dc6f29208e25eb8
Arie De Bok <arie@directdebit.co.za>
2023-05-02 15:25:22 +0200

    Add ze rand to cent confersion

--- stdext/currency.go
+++ stdext/currency.go
@@ -1,5 +1,11 @@
 package stdext
 
+import "math"
+
 func CentToRand(cent int) float64 {
 	return RoundTo(float64(cent)/100, 2)
+}
+
+func RandToCent(rand float64) int {
+	return int(math.Round(rand * 100))
 }
