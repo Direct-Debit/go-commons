a123afef9f8fd30bbac95501f3faeb66da6f5f58
UgandanSupaSoldier <jonathan@directdebit.co.za>
2023-08-16 10:00:15 +0200

    Allow format strings in error wrapper

--- stdext/errors.go
+++ stdext/errors.go
@@ -2,9 +2,12 @@ package stdext
 
 import "fmt"
 
-func WrapError(err error, message string) error {
+// WrapError wraps a given error with a message and returns it.
+// If the given error is nil, nil is also returned.
+// The message may be a format string with the arguments specified in a.
+func WrapError(err error, message string, a ...any) error {
 	if err == nil {
 		return nil
 	}
-	return fmt.Errorf("%s: %w", message, err)
+	return fmt.Errorf("%s: %w", fmt.Sprintf(message, a...), err)
 }
