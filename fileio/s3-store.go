e5c7466bdf97762c41ae4cf02ed2c944a78ead95
Ilian <ilian@teamgeek.io>
2026-01-13 16:38:27 +0200

    feat: allow s3 save to accept a content length param

--- fileio/s3-store.go
+++ fileio/s3-store.go
@@ -32,14 +32,15 @@ func NewS3Store(bucket string) S3Store {
 }
 
 func (s S3Store) Save(path string, content string) error {
-	return s.SaveStream(path, strings.NewReader(content))
+	return s.SaveStream(path, strings.NewReader(content), nil)
 }
 
-func (s S3Store) SaveStream(path string, content io.ReadSeeker) error {
+func (s S3Store) SaveStream(path string, content io.ReadSeeker, contentLength *int64) error {
 	_, err := s.s3.PutObject(&s3.PutObjectInput{
-		Body:   content,
-		Bucket: s.Bucket,
-		Key:    aws.String(path),
+		Body:          content,
+		Bucket:        s.Bucket,
+		Key:           aws.String(path),
+		ContentLength: contentLength,
 	})
 	if errlib.ErrorError(err, "Couldn't save object to "+path) {
 		return err
